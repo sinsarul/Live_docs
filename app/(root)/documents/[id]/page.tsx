902b68b913c5a41ecbb0bead96f51a628a261104
sinsar11456 <sinsarsanu111@gmail.com>
2024-07-21 20:19:09 +0530

    editor page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 const Documents = () => {
   return (
-    <div>Documents</div>
-  )
-}
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+        <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
 
-export default Documents
+export default Documents;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import { Inter as FontSans } from "next/font/google"
-import './globals.css'
-import { cn } from "@/lib/utils"
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'LiveDocs',
-  description: 'your go-to collaborative editor',
-}
+  title: "LiveDocs",
+  description: "your go-to collaborative editor",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode } ) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-    
       <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
-       {children}
+        {children}
       </body>
     </html>
-  )
+  );
 }
